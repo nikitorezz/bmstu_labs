b974ae3e34c62a5a8901164e78a74b68f1e24a6a
nikitorezz <nikitorezz@gmail.com>
2017-03-20 19:51:26 +0300

    Create CastException.h

---
+++ mod2_lab1/CastException.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <exception>
+#include <string>
+
+class CastException
+	: public std::exception{
+};
+
+class Owerflow
+	: public CastException{
+
+	void what(){
+		std::cout << "Ovwerflow exception" << std::endl;;
+	}
+};
+
+class WrongSymbol
+	: public CastException{
+
+	void what(){
+		std::cout << "WrongSymbol exception" << std::endl;
+	}
+};
